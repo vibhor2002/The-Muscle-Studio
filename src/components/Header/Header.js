edaa92bd76627e01b726695cefb91f9efc3b66eb
Vibhor Grover <grovervibhor21@gmail.com>
2022-07-11 13:35:42 +0530

    menu and ad done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,15 @@ import Logo from '../../assets/logo.png'
 const Header = () => {
   return (
     <div className="header">
-        <img src={Logo} alt="" />
+        <img className='logo' src={Logo} alt="" />
+        
+        <ul className='header-menu'>
+          <li>Home</li>
+          <li>Programs</li>
+          <li>Why us</li>
+          <li>Plans</li>
+          <li>Testimonials</li>
+        </ul>
     </div>
   )
 }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,25 @@ import Header from '../Header/Header'
 const Hero = () => {
   return (
     <div className="hero">
-        <div className="left-h">
-            <Header/>
+      <div className="left-h">
+        <Header />
+
+        <div className="the-best-ad">
+          <div></div>
+          <span>The best fitness club in the town</span>
         </div>
-        <div className="right-h">right side</div>
+
+        <div className="hero-text">
+          <div>
+            <span>Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+        </div>
+      </div>
+      <div className="right-h">right side</div>
     </div>
   )
 }
