c21035af71c791a5756029c2b68ee972ce273d37
Vibhor Grover <grovervibhor21@gmail.com>
2022-07-12 23:53:05 +0530

    Some front end

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -20,8 +20,34 @@ const Hero = () => {
           <div>
             <span>Ideal body</span>
           </div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and live up your life to fullest
+            </span>
+          </div>
+        </div>
+
+        {/* figures */}
+        <div className="figures">
+          <div>
+            <span>+ 140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+ 978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+ 50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+        <div className="hero-buttons">
+          <button className="btn"> Get Started</button>
+          <button className="btn">Learn More</button>
         </div>
       </div>
+
       <div className="right-h">right side</div>
     </div>
   )
